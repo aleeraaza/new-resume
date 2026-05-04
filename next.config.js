4ea64320a0ceb47001577f341b2b848c722d445e
aleeraaza <kaliraza755@gmail.com>
2026-05-04 15:03:35 +0500

    adding redirects to my site

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     // unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://new-resume-smoky.vercel.app/:path*",
+        permanent: true,
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
